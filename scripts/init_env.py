76a2d863481bc5545e56b00ed3e36aa7033e4cd3
Gopi Krishna Maganti <gopi.maganti1998@gmail.com>
2025-07-15 12:11:03 -0400

    Update workflows and add cloudformation

---
+++ scripts/init_env.py
@@ -0,0 +1,15 @@
+import boto3
+import json
+import os
+
+def write_env_file(secret_id: str = "fruitstore/db_credentials", region: str = "us-east-1"):
+    client = boto3.client("secretsmanager", region_name=region)
+    secret = client.get_secret_value(SecretId=secret_id)
+    secrets_dict = json.loads(secret["SecretString"])
+
+    with open(".env", "w") as f:
+        for key, value in secrets_dict.items():
+            f.write(f"{key}={value}\n")
+
+if __name__ == "__main__":
+    write_env_file()
