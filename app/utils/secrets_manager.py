1f52efd4e00a8e6bb1d7f822608e2639c4b2678c
Gopi Krishna Maganti <gopi.maganti1998@gmail.com>
2025-07-22 23:28:59 -0400

    Update github workflow and secret_manager

---
+++ app/utils/secrets_manager.py
@@ -0,0 +1,31 @@
+import boto3
+import json
+
+def create_secret():
+    secret_name = "fruitstore/db_credentials"
+    region_name = "us-east-1"
+
+    secret_value = {
+        "DB_USER": "fruituser",
+        "DB_PASS": "SuperSecurePass123",
+        "DB_HOST": "fruitstore-cluster.cluster-c69cq4mcm794.us-east-1.rds.amazonaws.com",
+        "DB_NAME": "fruitstore"
+    }
+
+    client = boto3.client('secretsmanager', region_name=region_name)
+
+    try:
+        client.create_secret(
+            Name=secret_name,
+            SecretString=json.dumps(secret_value)
+        )
+        print("✅ Secret created.")
+    except client.exceptions.ResourceExistsException:
+        print("⚠️ Secret exists. Updating...")
+        client.put_secret_value(
+            SecretId=secret_name,
+            SecretString=json.dumps(secret_value)
+        )
+        print("✅ Secret updated.")
+
+create_secret()
